86caa86cd15714dba087a6383199d98bdd2aea80
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2020-07-08 11:32:25 +0300

    process zero-sized maps on Windows correctly

--- src/appender.rs
+++ src/appender.rs
@@ -14,11 +14,20 @@ pub(crate) struct Appender {
     file: File,
     // This is used to trick the compiler so that we have parallel reads and
     // writes.
-    mmap: UnsafeCell<MmapMut>,
+    mmap: UnsafeCell<MaybeMmap>,
     // Atomic is used to ensure that we can have lock-free and memory-safe
     // reads. Since this value is updated only after the write has finished it
     // is safe to use it as the upper boundary for reads.
     actual_size: AtomicUsize,
+}
+
+/// This enum is required to handle a special case: on Windows we cannot have a
+/// zero-sized memory map, so we need to have a file of non-zero length first.
+// TODO relace this with a union when the corresponding RFC is stabilized.
+// https://github.com/rust-lang/rust/issues/55149
+enum MaybeMmap {
+    Mmap(MmapMut),
+    Uninit(usize),
 }
 
 impl Appender {
@@ -49,12 +58,11 @@ impl Appender {
             return Err(Error::MmapTooSmall);
         }
 
-        let mmap = UnsafeCell::new(unsafe {
-            MmapOptions::new()
-                .len(map_size)
-                .map_mut(&file)
-                .map_err(|err| Error::Mmap(path.to_path_buf(), err))?
-        });
+        let mut mmap = MaybeMmap::new_uninit(map_size);
+        if actual_size > 0 {
+            mmap.init(&file)?;
+        }
+        let mmap = UnsafeCell::new(mmap);
 
         let actual_size = AtomicUsize::from(actual_size);
 
@@ -72,11 +80,11 @@ impl Appender {
     where
         F: Fn(&mut [u8]),
     {
-        let mmap = unsafe { self.mmap.get().as_mut().unwrap() };
+        let maybe_mmap = unsafe { self.mmap.get().as_mut().unwrap() };
         let actual_size = self.actual_size.load(Ordering::Relaxed);
 
         let new_file_size = actual_size + size_inc;
-        if mmap.len() < new_file_size {
+        if maybe_mmap.len() < new_file_size {
             return Err(Error::MmapTooSmall);
         }
 
@@ -84,6 +92,9 @@ impl Appender {
             self.file
                 .set_len(new_file_size as u64)
                 .map_err(Error::Write)?;
+            maybe_mmap.init(&self.file)?;
+
+            let mmap = maybe_mmap.as_mut().unwrap();
 
             f(&mut mmap[actual_size..new_file_size]);
 
@@ -110,7 +121,7 @@ impl Appender {
     where
         F: Fn(&'a [u8]) -> Option<U>,
     {
-        let mmap = unsafe { self.mmap.get().as_ref().unwrap() };
+        let mmap = unsafe { self.mmap.get().as_ref().unwrap() }.as_ref()?;
         let actual_size = self.actual_size.load(Ordering::Relaxed);
 
         f(&mmap[0..actual_size])
@@ -122,13 +133,55 @@ impl Appender {
 
     /// Get the pointer to the underlying raw data.
     pub fn snapshot(&self) -> &[u8] {
-        let mmap = unsafe { self.mmap.get().as_ref().unwrap() };
+        let mmap = unsafe { self.mmap.get().as_ref().unwrap() }
+            .as_ref()
+            .unwrap_or(&[]);
         let size = self.actual_size.load(Ordering::Relaxed);
         &mmap[..size]
     }
 }
 
 unsafe impl Sync for Appender {}
+
+impl MaybeMmap {
+    fn new_uninit(map_size: usize) -> Self {
+        MaybeMmap::Uninit(map_size)
+    }
+
+    fn init(&mut self, file: &File) -> Result<(), Error> {
+        match self {
+            MaybeMmap::Mmap(_) => {}
+            MaybeMmap::Uninit(map_size) => {
+                *self = MaybeMmap::Mmap(
+                    unsafe { MmapOptions::new().len(*map_size).map_mut(&file) }
+                        .map_err(|err| Error::Mmap(err))?,
+                );
+            }
+        }
+        Ok(())
+    }
+
+    fn as_ref(&self) -> Option<&[u8]> {
+        match self {
+            MaybeMmap::Mmap(mmap) => Some(mmap),
+            MaybeMmap::Uninit(_) => None,
+        }
+    }
+
+    fn as_mut(&mut self) -> Option<&mut MmapMut> {
+        match self {
+            MaybeMmap::Mmap(mmap) => Some(mmap),
+            MaybeMmap::Uninit(_) => None,
+        }
+    }
+
+    fn len(&self) -> usize {
+        match self {
+            MaybeMmap::Mmap(mmap) => mmap.len(),
+            MaybeMmap::Uninit(map_size) => *map_size,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ pub enum Error {
     /// Failed to open file.
     FileOpen(PathBuf, io::Error),
     /// Failed to create mmap.
-    Mmap(PathBuf, io::Error),
+    Mmap(io::Error),
     // mmap is too small for a file to be extended.
     MmapTooSmall,
     /// Error while extending a file.
@@ -24,7 +24,7 @@ impl error::Error for Error {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Error::FileOpen(_, source) => Some(source),
-            Error::Mmap(_, source) => Some(source),
+            Error::Mmap(source) => Some(source),
             Error::MmapTooSmall => None,
             Error::Write(source) => Some(source),
             Error::PathNotDir => None,
@@ -39,7 +39,7 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::FileOpen(path, _) => write!(f, "failed to open file `{}`", path.display()),
-            Error::Mmap(path, _) => write!(f, "memory map failed for file `{}`", path.display()),
+            Error::Mmap(_) => write!(f, "memory map failed"),
             Error::MmapTooSmall => write!(f, "the map size is too little to write new records"),
             Error::Write(_) => write!(f, "failed to write the file"),
             Error::PathNotDir => write!(
